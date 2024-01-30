4c77aa27147a1723e1e33052fa640bb8b9c1e2ea
zhongzhouTan <cqyzdp1@163.com>
2024-01-30 23:37:20 +0800

    fix: just add workspace for outer cago toml

--- saml2/src/core/authn_request.rs
+++ saml2/src/core/authn_request.rs
@@ -56,34 +56,42 @@ impl AuthnRequest {
     const CHILD_REQUESTED_AUTHN_CONTEXT_NAME: &'static str = "RequestedAuthnContext";
     const CHILD_SCOPING_NAME: &'static str = "Scoping";
 
+    #[inline]
     pub fn subject(&self) -> Option<&Subject> {
         self.subject.as_ref()
     }
 
+    #[inline]
     pub fn set_subject(&mut self, subject: Option<Subject>) {
         self.subject = subject
     }
 
+    #[inline]
     pub fn name_id_policy(&self) -> Option<&NameIDPolicy> {
         self.name_id_policy.as_ref()
     }
 
+    #[inline]
     pub fn set_name_id_policy(&mut self, name_id_policy: Option<NameIDPolicy>) {
         self.name_id_policy = name_id_policy
     }
 
+    #[inline]
     pub fn conditions(&self) -> Option<&Conditions> {
         self.conditions.as_ref()
     }
 
+    #[inline]
     pub fn set_conditions(&mut self, conditions: Option<Conditions>) {
         self.conditions = conditions
     }
 
+    #[inline]
     pub fn requested_authn_context(&self) -> Option<&RequestedAuthnContext> {
         self.requested_authn_context.as_ref()
     }
 
+    #[inline]
     pub fn set_request_authn_context(
         &mut self,
         request_authn_context: Option<RequestedAuthnContext>,
@@ -91,34 +99,42 @@ impl AuthnRequest {
         self.requested_authn_context = request_authn_context
     }
 
+    #[inline]
     pub fn scoping(&self) -> Option<&Scoping> {
         self.scoping.as_ref()
     }
 
+    #[inline]
     pub fn set_scoping(&mut self, scoping: Option<Scoping>) {
         self.scoping = scoping;
     }
 
+    #[inline]
     pub fn force_authn(&self) -> Option<bool> {
         self.force_authn
     }
 
+    #[inline]
     pub fn set_force_authn(&mut self, force_authn: Option<bool>) {
         self.force_authn = force_authn;
     }
 
+    #[inline]
     pub fn is_passive(&self) -> Option<bool> {
         self.is_passive
     }
 
+    #[inline]
     pub fn set_is_passive(&mut self, is_passive: Option<bool>) {
         self.is_passive = is_passive;
     }
 
+    #[inline]
     pub fn assertion_consumer_service_index(&self) -> Option<usize> {
         self.assertion_consumer_service_index
     }
 
+    #[inline]
     pub fn set_assertion_consumer_service_index(
         &mut self,
         assertion_consumer_service_index: Option<usize>,
@@ -126,10 +142,12 @@ impl AuthnRequest {
         self.assertion_consumer_service_index = assertion_consumer_service_index;
     }
 
+    #[inline]
     pub fn assertion_consumer_service_url(&self) -> Option<&String> {
         self.assertion_consumer_service_url.as_ref()
     }
 
+    #[inline]
     pub fn set_assertion_consumer_service_url(
         &mut self,
         assertion_consumer_service_url: Option<String>,
@@ -137,18 +155,22 @@ impl AuthnRequest {
         self.assertion_consumer_service_url = assertion_consumer_service_url;
     }
 
+    #[inline]
     pub fn protocol_binding(&self) -> Option<&String> {
         self.protocol_binding.as_ref()
     }
 
+    #[inline]
     pub fn set_protocol_binding(&mut self, protocol_binding: Option<String>) {
         self.protocol_binding = protocol_binding;
     }
 
+    #[inline]
     pub fn attribute_consuming_service_index(&self) -> Option<usize> {
         self.attribute_consuming_service_index
     }
 
+    #[inline]
     pub fn set_attribute_consuming_service_index(
         &mut self,
         attribute_consuming_service_index: Option<usize>,
@@ -156,76 +178,94 @@ impl AuthnRequest {
         self.attribute_consuming_service_index = attribute_consuming_service_index;
     }
 
+    #[inline]
     pub fn provider_name(&self) -> Option<&String> {
         self.provider_name.as_ref()
     }
 
+    #[inline]
     pub fn set_provider_name(&mut self, provider_name: Option<String>) {
         self.provider_name = provider_name;
     }
 }
 
 impl RequestAbstractType for AuthnRequest {
+    #[inline]
     fn version(&self) -> &SAMLVersion {
         &self.version
     }
 
+    #[inline]
     fn set_version(&mut self, saml_version: SAMLVersion) {
         self.version = saml_version
     }
 
+    #[inline]
     fn id(&self) -> &String {
         &self.id
     }
 
+    #[inline]
     fn set_id(&mut self, id: String) {
         self.id = id;
     }
 
+    #[inline]
     fn issue_instant(&self) -> &DateTime<Utc> {
         &self.issue_instant
     }
 
+    #[inline]
     fn set_issue_instant(&mut self, issue_instant: DateTime<Utc>) {
         self.issue_instant = issue_instant
     }
 
+    #[inline]
     fn destination(&self) -> Option<&String> {
         self.destination.as_ref()
     }
 
+    #[inline]
     fn set_destination(&mut self, destination: Option<String>) {
         self.destination = destination
     }
 
+    #[inline]
     fn consent(&self) -> Option<&String> {
         self.consent.as_ref()
     }
 
+    #[inline]
     fn set_consent(&mut self, consent: Option<String>) {
         self.consent = consent
     }
 
+    #[inline]
     fn issuer(&self) -> Option<&Issuer> {
         self.issuer.as_ref()
     }
 
+    #[inline]
     fn set_issuer(&mut self, issuer: Option<Issuer>) {
         self.issuer = issuer
     }
 
+    #[inline]
     fn extensions(&self) -> Option<&Extensions> {
         self.extensions.as_ref()
     }
 
+    #[inline]
     fn set_extensions(&mut self, extensions: Option<Extensions>) {
         self.extensions = extensions
     }
 
+    #[inline]
     fn signature(&self) -> Option<&String> {
         self.signature.as_ref()
     }
 
+    #[inline]
     fn set_signature(&mut self, signature: Option<String>) {
         self.signature = signature
     }
@@ -262,6 +302,7 @@ impl TryFrom<Ref<'_, XmlObject>> for AuthnRequest {
     type Error = SAMLError;
 
     fn try_from(element: Ref<'_, XmlObject>) -> Result<Self, Self::Error> {
+        #[inline]
         fn parse_from_string<T: FromStr>(xml_string: &str) -> Result<T, SAMLError> {
             xml_string
                 .parse::<T>()
